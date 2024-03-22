cde55f57d18af8a3f638c76043405b2169a8a3b7
ArrayIterator <arrayiterator@gmail.com>
2024-03-22 17:00:51 +0700

    update

--- public/index.php
+++ public/index.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 namespace Pentagonal\Sso\Public;
 
 // doing
+use Pentagonal\Sso\Core\Service;
+use function define;
 use function dirname;
 
-require dirname(__DIR__) .'/vendor/autoload.php';
+return (function () {
+    require dirname(__DIR__) .'/vendor/autoload.php';
 
-// process routing
+    // phpcs:disable PSR1.Files.SideEffects
+    define('PUBLIC_PATH', __DIR__);
+    define('ROOT_PATH', dirname(__DIR__));
+
+    $service = new Service();
+    return $service->run();
+})();
