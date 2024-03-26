87111e078510b23553127d37d9162ccdc00666b5
ArrayIterator <arrayiterator@gmail.com>
2024-03-27 06:24:57 +0700

    fix

--- loader.php
+++ loader.php
@@ -11,6 +11,7 @@ return (function () {
     // phpcs:disable PSR1.Files.SideEffects
     define('ROOT_PATH', __DIR__);
     define('APP_PATH', \ROOT_PATH . '/app');
+
     require __DIR__ .'/vendor/autoload.php';
 
     return new Service();
